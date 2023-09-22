c198df4ed8cc1107c227240e18bd1ec1082c867a
sailendra131 <sailendra.n.jena@gmail.com>
2023-09-23 01:30:17 +0530

    Seventh Commit

--- src/test/java/com/jenkin/core/api/SpringJenkinExampleApplicationTests.java
+++ src/test/java/com/jenkin/core/api/SpringJenkinExampleApplicationTests.java
@@ -21,6 +21,7 @@ class SpringJenkinExampleApplicationTests {
 		log.info("Test Case executed Successfully Forth time.....");
 		log.info("Test Case executed Successfully Fifth time.....");
 		log.info("Test Case executed Successfully Sixth time.....");
+		log.info("Test Case executed Successfully Seventh time.....");
 		
 		Assertions.assertEquals(true, true);
 	}
